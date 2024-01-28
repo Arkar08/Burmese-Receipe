f1afb54e5bae784ee5b927d7a552d4191438c7ab
Soe Arkar Maung <148836349+Arkar08@users.noreply.github.com>
2024-01-28 11:26:53 +0630

    second props

--- src/Home.jsx
+++ src/Home.jsx
@@ -14,7 +14,7 @@ const Home = ({data}) => {
                     return(
                         <div className="box" key={id}>
                         <div className="image">
-                            <img src="https://images.unsplash.com/photo-1546069901-ba9599a7e63c?q=80&w=580&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" alt="image1" />
+                            <img src={d.Image} alt="image1" />
                         </div>
                         <div className="text">
                             <h2>{d.Name}</h2>
